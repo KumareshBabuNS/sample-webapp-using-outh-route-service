3d6a9cb1d0c84e5396ec1c8e65e4cebf735c7a27
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-03-08 15:10:17 -0800

    Added in OAuth2 config - broken still though

--- src/main/java/io/pivotalservices/Application.java
+++ src/main/java/io/pivotalservices/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleWebappUsingOuthRouteServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleWebappUsingOuthRouteServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
