643382739ad2a52361594ebe79ac162bf65b7c47
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-03-08 16:27:30 -0800

    Fixed failing test

--- src/test/java/io/pivotalservices/ApplicationTests.java
+++ src/test/java/io/pivotalservices/ApplicationTests.java
@@ -1,9 +1,14 @@
 package io.pivotalservices;
 
+import io.pivotalservices.config.GeneralConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -37,7 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
 @WebAppConfiguration
 public class ApplicationTests {
 
@@ -204,4 +209,14 @@ public class ApplicationTests {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
+	@Import(GeneralConfig.class)
+	@Configuration
+	@EnableAutoConfiguration
+	public static class TestConfig {
+		
+		@Bean
+		public RequestForwardingController requestForwardingController(RestTemplate restTemplate) {
+			return new RequestForwardingController(restTemplate);
+		}
+	}
 }
